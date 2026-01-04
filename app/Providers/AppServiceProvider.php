ec2c817c947e36e02019533777d5452943ee3633
Chelsea Deanna Husin <chelseadh07@gmail.com>
2026-01-04 22:18:23 +0700

    auto create sqlite file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,10 @@
-<?php
+<?php 
 
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\File;
 
 use App\Http\Middleware\BuyerOnly;
 use App\Http\Middleware\ArtistOnly;
@@ -29,5 +30,13 @@ class AppServiceProvider extends ServiceProvider
         $router->aliasMiddleware('buyer', BuyerOnly::class);
         $router->aliasMiddleware('artist', ArtistOnly::class);
         $router->aliasMiddleware('admin', AdminOnly::class);
+
+        // ===============================
+        // AUTO CREATE SQLITE DATABASE FILE
+        // ===============================
+        $dbPath = database_path('database.sqlite'); // path ke database
+        if (!File::exists($dbPath)) {
+            File::put($dbPath, ''); // buat file kosong
+        }
     }
 }
